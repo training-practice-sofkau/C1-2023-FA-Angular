acabc95e39329c60c384b9fa0ab57f782923c617
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-15 18:26:18 -0500

    course form implemented

--- src/app/components/forms/course-form/course-form.component.ts
+++ src/app/components/forms/course-form/course-form.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, KeyValueDiffers, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { CourseService } from 'src/app/services/course-service/course.service';
+import { Course } from 'src/app/models/course.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-form',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class CourseFormComponent {
 
+  courseForm: FormGroup = new FormGroup({});
+
+  constructor(private builder: FormBuilder,
+    private service: CourseService,
+    private route: ActivatedRoute){
+    }
+
+    ngOnInit(): void {
+      this.courseForm = this.builder.group(
+        {
+          name: '',
+          coach: '',
+          level: 0,
+        }
+      );
+
+      this.route.queryParams.subscribe((info) => {
+        if(JSON.stringify(info) !== JSON.stringify({})){
+          this.courseForm.setValue({
+            name: JSON.parse(info['data']).name,
+            coach: JSON.parse(info['data']).coach,
+            level: JSON.parse(info['data']).level,
+           })
+
+        }
+
+
+      } )
+
+
+    }
 }
