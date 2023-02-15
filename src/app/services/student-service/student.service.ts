5be72d2e75cfc08bbdf451a0476120f217c39600
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-15 17:26:08 -0500

    Main CRUD http services declared for student

--- src/app/services/student-service/student.service.ts
+++ src/app/services/student-service/student.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from 'src/app/models/student.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  api: string = "http://localhost:8080/api/students"
+
+  getAll(): Observable<any> {
+    return this.http.get(this.api);
+  }
+
+  getByID(param: string): Observable<any> {
+    return this.http.get(this.api+"/"+param);
+  }
+
+  post(student:Student):  Observable<any> {
+    return this.http.post(this.api, student);
+  }
+
+  update(param: string, student:Student): Observable<any> {
+    return this.http.put(this.api+"/"+param, student);
+  }
+
+  deleteByID(param: string):  Observable<any> {
+    return this.http.delete(this.api+"/"+param);
+  }
+
 }
