1fce9fe5551fb9c153d7c532bf5a8f9d2d6fae1e
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-15 17:42:31 -0500

    student model modified to match the backend student entity

--- src/app/pages/student-page/student-page.component.ts
+++ src/app/pages/student-page/student-page.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student-service/student.service';
 
 @Component({
   selector: 'app-student-page',
   templateUrl: './student-page.component.html',
   styleUrls: ['./student-page.component.scss']
 })
-export class StudentPageComponent {
-  l_students: Student[] = [{
+export class StudentPageComponent implements OnInit{
+
+  l_students: Student[] = [];
+
+  /* l_students: Student[] = [{
     id: "1",
     name: "Mary Rojas",
     idNum: "1234567890",
@@ -22,9 +26,22 @@ export class StudentPageComponent {
     age: 55,
     mail: "sarah.vargas@hotmail.com",
     numCourses: 2
-  }];
-  
+  }]; */
+
   total: number = this.l_students.length;
-  
+
+  constructor(private service: StudentService) { }
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe({
+      next: (students) => {
+        this.l_students = students.data,
+        console.log(students.data)
+        this.total = this.l_students.length;
+      },
+      error: (console.log),
+      complete: (console.log)
+    })
+  }
 
 }
