73d07d8b0509ac300767609e79786f86e1f7a068
BrianSammit <briansammit.cruz@gmail.com>
2023-02-16 02:07:05 -0500

    Add course form

--- src/app/components/forms/course-form/course-form.component.ts
+++ src/app/components/forms/course-form/course-form.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, KeyValueDiffers, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-form',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent {
+  courseForm: FormGroup = new FormGroup({});
+
+  constructor(private builder: FormBuilder, 
+    private service: CourseService, 
+    private route: ActivatedRoute){
+     
+    }
+
+  ngOnInit(): void {
+    this.courseForm = this.builder.group(
+      {
+        name: '',
+        coach: '',
+        level: 0,
+      }
+    );
+
+    this.route.queryParams.subscribe((info) => {
+      if(JSON.stringify(info) !== JSON.stringify({})){
+        this.courseForm.setValue({
+          name: JSON.parse(info['data']).name,
+          coach: JSON.parse(info['data']).coach,
+          level: JSON.parse(info['data']).level,
+         })
+
+      }
+        
+
+    } )
+
+
+  }
 
 }
