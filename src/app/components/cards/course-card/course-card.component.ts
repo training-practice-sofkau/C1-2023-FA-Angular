43de43295791820ef5e51464f133dd5ddf106c88
Juangmcz <juangui-089@hotmail.com>
2023-02-16 13:47:02 -0500

    course-card was styled and implemented.

--- src/app/components/cards/course-card/course-card.component.ts
+++ src/app/components/cards/course-card/course-card.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.scss']
+  styleUrls: ['./course-card.component.scss'],
 })
 export class CourseCardComponent {
+  @Input() course: Course = {
+    courseId: '',
+    name: '',
+    coach: '',
+    level: 0,
+    lastUpdated: new Date(),
+    students: [],
+  };
+  constructor(private router: Router, private service: CourseService) {}
 
+  goToForm() {
+    this.router.navigate(['courses/edit'], {
+      queryParams: {
+        data: JSON.stringify(this.course),
+      },
+    });
+  }
+
+  deleteCourse() {
+    if (confirm('Are you sure to delete the course?')) {
+      this.service.deleteCourse(this.course.courseId).subscribe({
+        error: console.log,
+        complete: console.log,
+      });
+      alert('The course was deleted');
+      window.location.reload();
+    }
+  }
 }
