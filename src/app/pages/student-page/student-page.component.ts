0ecb11b5c1e472b27a871bdc6543a01474fc2495
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-16 09:00:06 -0500

    Pagination added to the student main page

--- src/app/pages/student-page/student-page.component.ts
+++ src/app/pages/student-page/student-page.component.ts
@@ -10,6 +10,7 @@ import { StudentService } from 'src/app/services/student-service/student.service
 export class StudentPageComponent implements OnInit{
 
   l_students: Student[] = [];
+  p: number = 1;
 
   /* l_students: Student[] = [{
     id: "1",
