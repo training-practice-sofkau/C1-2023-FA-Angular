45724a97f8393edf82be5f0eb72dc9ace88dce71
isan-san <isan.9.9f@gmail.com>
2023-02-16 10:57:11 -0500

    Delete button from course is working

--- src/app/components/cards/course-card/course-card.component.ts
+++ src/app/components/cards/course-card/course-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { Course } from 'src/app/models/course.model';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-card',
@@ -8,7 +9,8 @@ import { Course } from 'src/app/models/course.model';
   styleUrls: ['./course-card.component.scss'],
 })
 export class CourseCardComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private courseService: CourseService) {}
+  @Output() reload: EventEmitter<any> = new EventEmitter();
   @Input() course: Course = {
     courseId: 'n/a',
     name: 'n/a',
@@ -24,5 +26,22 @@ export class CourseCardComponent {
         data: JSON.stringify(this.course),
       },
     });
+  }
+  onDelete() {
+    if (confirm('Course ' + this.course.name + ' Will be deleted')) {
+      this.courseService.delete(this.course.courseId).subscribe({
+        next: (course) => {
+          if (course) {
+            alert('Course deleted');
+            this.reload.emit('Course deleted');
+          } else {
+            alert("Course hasn't been deleted");
+            this.reload.emit("Course hasn't been deleted");
+          }
+        },
+        error: console.log,
+        complete: console.log,
+      });
+    }
   }
 }
