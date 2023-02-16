2ce6473f279ba40d88305779c6bb30b52abcc27f
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-02-16 08:09:13 -0500

    Course page show the list of courses

--- src/app/pages/course-page/course-page.component.ts
+++ src/app/pages/course-page/course-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Course} from "../../models/course.model";
 
 @Component({
   selector: 'app-course-page',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-page.component.scss']
 })
 export class CoursePageComponent {
+
+  l_courses: Course[] = [{
+    id: "1",
+    name: "Java",
+    coach: "Jonathan",
+    level: 3,
+    lastUpdated: new Date(2023),
+    studentList: [{
+      id: "1",
+      name: "Mary Rojas",
+      idNum: "1234567890",
+      age: 25,
+      mail: "mary.rojas@hotmail.com",
+      numCourses: 0
+    }]
+  },
+    {
+      id: "2",
+      name: "Angular",
+      coach: "Daniel",
+      level: 2,
+      lastUpdated: new Date(2022),
+      studentList: []
+    }];
+
+  total: number = this.l_courses.length;
 
 }
