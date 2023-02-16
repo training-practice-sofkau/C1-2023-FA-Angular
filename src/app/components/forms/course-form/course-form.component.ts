c975026c21bdaaf220ba163648ce337f00a03125
Difsan <difsanchezru@gmail.com>
2023-02-16 01:56:27 -0500

    courseForm style was fixed

--- src/app/components/forms/course-form/course-form.component.ts
+++ src/app/components/forms/course-form/course-form.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+
+  courseForm: FormGroup = new FormGroup({});
+
+  constructor(private builder: FormBuilder,
+    private service: CourseService,
+    private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    this.courseForm = this.builder.group(
+      {
+        name: '',
+        coach: '',
+        level: 0,
+        lastUpdated: new Date(0)
+      },
+    );
+
+    this.route.queryParams.subscribe((info) => {
+      if(JSON.stringify(info) !== JSON.stringify({})){
+        this.courseForm.setValue({
+          name: JSON.parse(info['data']).name,
+        coach: JSON.parse(info['data']).coach,
+        level: JSON.parse(info['data']).level,
+        lastUpdated: JSON.parse(info['data']).lastUpdated,
+        })
+      }
+    })
+  }
 
 }
