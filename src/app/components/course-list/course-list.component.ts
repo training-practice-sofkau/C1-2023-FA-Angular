5b6829025e3f37578a87c3816547112c480e805b
Difsan <difsanchezru@gmail.com>
2023-02-16 02:03:36 -0500

    courselist style was changed

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
 
 @Component({
   selector: 'app-course-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent {
-
+  c_founded: Course[] = [];
+  founded: number = 0;
+  searchingBy: string = '';
+  p: number = 0;
 }
