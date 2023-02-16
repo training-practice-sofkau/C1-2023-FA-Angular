f7831af528260c80a73d25737819840c34056464
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-02-16 08:54:53 -0500

    Course card and page styled and working

--- src/app/pages/course-page/course-page.component.ts
+++ src/app/pages/course-page/course-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Course} from "../../models/course.model";
+import {formatDate, getLocaleDateFormat} from "@angular/common";
 
 @Component({
   selector: 'app-course-page',
@@ -13,14 +14,22 @@ export class CoursePageComponent {
     name: "Java",
     coach: "Jonathan",
     level: 3,
-    lastUpdated: new Date(2023),
+    lastUpdated: new Date("January 25, 2023"),
     studentList: [{
       id: "1",
       name: "Mary Rojas",
       idNum: "1234567890",
       age: 25,
       mail: "mary.rojas@hotmail.com",
-      numCourses: 0
+      numCourses: 1
+    },
+    {
+      id: "3",
+      name: "Nicolas Pinilla",
+      idNum: "12345678928",
+      age: 20,
+      mail: "nico.pinilla@hotmail.com",
+      numCourses: 1
     }]
   },
     {
@@ -28,7 +37,7 @@ export class CoursePageComponent {
       name: "Angular",
       coach: "Daniel",
       level: 2,
-      lastUpdated: new Date(2022),
+      lastUpdated: new Date("December 25, 2022"),
       studentList: []
     }];
 
