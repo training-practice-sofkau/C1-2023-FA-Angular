455291f3c5c28e60e86a62da7481487fed806dae
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-16 08:45:38 -0500

    Delete operation implemented in the student card component

--- src/app/components/cards/student-card/student-card.component.ts
+++ src/app/components/cards/student-card/student-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student-service/student.service';
 import { StudentFormComponent } from '../../forms/student-form/student-form.component';
 
 @Component({
@@ -9,13 +10,26 @@ import { StudentFormComponent } from '../../forms/student-form/student-form.comp
   styleUrls: ['./student-card.component.scss']
 })
 export class StudentCardComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: StudentService) { }
   @Input() student: Student = {
     studentId: '',
     name: '',
     idNum: '',
     age: 0,
     mail: '',
+  }
+
+
+  onDelete(): void {
+    if (confirm(`Do you want to delete the student with ID: ${this.student?.studentId}?`)) {
+      if (this.student) {
+        this.service.deleteByID(<string>this.student?.studentId).subscribe((answer) => {
+          //Fix the server response
+          console.log(answer)
+          alert(`Student with ID: ${this.student?.studentId} has been deleted!`)
+        })
+      }
+    }
   }
 
   goToForm(){
