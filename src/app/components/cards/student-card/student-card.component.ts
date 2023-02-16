ac179403dcd3c2fa14f96a81eca55d01313bd277
Juangmcz <juangui-089@hotmail.com>
2023-02-16 13:18:49 -0500

    Student card is working as expected.

--- src/app/components/cards/student-card/student-card.component.ts
+++ src/app/components/cards/student-card/student-card.component.ts
@@ -1,33 +1,40 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student-service/student.service';
 import { StudentFormComponent } from '../../forms/student-form/student-form.component';
 
 @Component({
   selector: 'app-student-card',
   templateUrl: './student-card.component.html',
-  styleUrls: ['./student-card.component.scss']
+  styleUrls: ['./student-card.component.scss'],
 })
 export class StudentCardComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: StudentService) {}
   @Input() student: Student = {
-    id: '',
+    studentId: '',
     name: '',
-    idNum: '',
+    idNumber: '',
     age: 0,
     mail: '',
-    numCourses: 0
+  };
+
+  goToForm() {
+    this.router.navigate(['students/edit'], {
+      queryParams: {
+        data: JSON.stringify(this.student),
+      },
+    });
   }
 
-  goToForm(){
-    this.router.navigate(['students/edit'],{
-      queryParams:{
-        data: JSON.stringify(this.student)
-
-      }
-      
-    })
+  deleteStudent() {
+    if (confirm('Are you sure to delete the student?')) {
+      this.service.deleteStudent(this.student.studentId).subscribe({
+        error: console.log,
+        complete: console.log,
+      });
+      alert('The student was deleted');
+      window.location.reload();
+    }
   }
-
-
 }
