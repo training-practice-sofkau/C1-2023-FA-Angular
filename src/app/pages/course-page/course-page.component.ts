003b393ccd1033e5a6e2f3453c78dabc89e189d8
andrscc <andrescc143@gmail.com>
2023-02-16 10:33:40 -0500

    The mocking data was added in the .ts file of the course page component

--- src/app/pages/course-page/course-page.component.ts
+++ src/app/pages/course-page/course-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { Student } from 'src/app/models/student.model';
 
 @Component({
   selector: 'app-course-page',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-page.component.scss']
 })
 export class CoursePageComponent {
+
+  l_courses: Course[] = [{
+    id: "1",
+    name: "Development [English]",
+    coach: "Mishell",
+    level: 1,
+    lastUpdated: new Date("2023-05-16"),
+    studentList: []
+  },
+{
+    id: "2",
+    name: "Development",
+    coach: "Ricardo Milos",
+    level: 1,
+    lastUpdated: new Date("2023-05-16"),
+    studentList: []
+  },
+  {
+    id: "3",
+    name: "Testing",
+    coach: "Another Ricardo Milos",
+    level: 1,
+    lastUpdated: new Date("2023-05-16"),
+    studentList: []
+  },
+  {
+    id: "4",
+    name: "Project Management",
+    coach: "Ricarda Milan",
+    level: 1,
+    lastUpdated: new Date("2023-05-16"),
+    studentList: []
+  },
+  {
+    id: "5",
+    name: "UI/UX",
+    coach: "Richard Milos",
+    level: 1,
+    lastUpdated: new Date("2023-05-16"),
+    studentList: []
+  },
+];
+
+  total: number = this.l_courses.length;
 
 }
