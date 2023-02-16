d02931d550c666b75512af34336b213a5143bbde
Diego Becerra <diegofer_1110@hotmail.com>
2023-02-15 21:18:51 -0500

    Making course service

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -1,10 +1,10 @@
 import { Student } from "./student.model";
 
 export interface Course {
-    id: string,
+    id?: string,
     name: string,
     coach: string,
     level: number,
-    lastUpdated: Date,
+    lastUpdated?: Date,
     studentList: Student[]
 }
--- src/app/services/course-service/course.service.ts
+++ src/app/services/course-service/course.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from 'src/app/models/course.model';
+import { envDev } from 'src/environment/envDev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService{
 
-  constructor() { }
+  api:string = envDev.url+"courses"
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any>{
+    return this.http.get(this.api)
+  }
+
+  updateCourse(course:Course):Observable<any>{
+    return this.http.put(this.api,course)
+  }
+
+  saveCourse(course:Course):Observable<any>{
+    return this.http.post(this.api,course)
+  }
+
+  deleteCourse(id:string):Observable<any>{
+    return this.http.delete(this.api+`/delete?courseId=${id}`,{responseType: 'text'})
+  }
+
 }
