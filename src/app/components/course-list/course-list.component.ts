7b6dd41ad940958afe14cd364e746831a6280cd5
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-02-16 09:59:48 -0500

    Small changes for organization

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Course} from "../../models/course.model";
 
 @Component({
   selector: 'app-course-list',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class CourseListComponent {
 
+  c_founded: Course[] = [];
+  founded: number = 0;
+  searchingBy: string = '';
+
+  fill(){
+    this.c_founded = [{
+      id: "2",
+      name: "Angular",
+      coach: "Daniel",
+      level: 2,
+      lastUpdated: new Date("December 25, 2022"),
+      studentList: []
+    }];
+
+    this.founded = 1;
+  }
 }
