96738ecbee91d0dcab21a9362dbc20c5e0c92341
BrianSammit <briansammit.cruz@gmail.com>
2023-02-16 15:24:59 -0500

    Connect backend and return all students

--- src/app/pages/student-page/student-page.component.ts
+++ src/app/pages/student-page/student-page.component.ts
@@ -1,59 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student-service/student.service';
 
 @Component({
   selector: 'app-student-page',
   templateUrl: './student-page.component.html',
   styleUrls: ['./student-page.component.scss']
 })
-export class StudentPageComponent {
+export class StudentPageComponent implements OnInit {
 
- page: number = 1;
+    constructor(private service: StudentService){}
 
-  l_students: Student[] = [
-      {
-          id: "1",
-          name: "Mary Rojas",
-          idNum: "1234567890",
-          age: 25,
-          mail: "mary.rojas@hotmail.com",
-          numCourses: 0
-      },
-      {
-          id: "2",
-          name: "Sarah Vargas",
-          idNum: "0987654321",
-          age: 55,
-          mail: "sarah.vargas@hotmail.com",
-          numCourses: 2
-      },
-      {
-          id: "3",
-          name: "Sarah Vargas",
-          idNum: "0987654321",
-          age: 55,
-          mail: "sarah.vargas@hotmail.com",
-          numCourses: 2
-      },
-      {
-          id: "4",
-          name: "Sarah Vargas",
-          idNum: "0987654321",
-          age: 55,
-          mail: "sarah.vargas@hotmail.com",
-          numCourses: 2
-      },
-      {
-          id: "5",
-          name: "Sarah Vargas",
-          idNum: "0987654321",
-          age: 55,
-          mail: "sarah.vargas@hotmail.com",
-          numCourses: 2
-      }
-];
+    @Input() param: string = "";
+
+    l_students: Student[] = [];
+    total: number = 0;
+    page: number = 1;
   
-  total: number = this.l_students.length;
-  
+    ngOnInit(): void {
+        this.fetchData();
+    }
+
+    fetchData(){
+        this.service.getAll().subscribe({
+            next: (student) => {
+                this.l_students = student;
+                    this.total = this.l_students.length;
+            },
+            error: (console.log),
+            complete: (console.log)
+        })
+    }
+
+
 
 }
