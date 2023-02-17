9f92b328cafe21d8b4a05c35439a91e0600a6569
William Serna <williamsernac@hotmail.com>
2023-02-17 04:03:38 -0500

    Filter for Courses

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent {
+  constructor(private service: CourseService){}
+  c_found: Course[] = [{
+    id: '',
+    name: '',
+    coach: '',
+    level: 0,
+    lastUpdated: new Date(),
+    studentList: [],
+  }];
+  found: number = 0;
+  searchingBy: string = '';
+  param: string = ''
 
+
+ngSearch(){
+  if(this.searchingBy == 'name'){
+    this.service.getByName(this.param).subscribe({
+      next: (course) => {
+        this.c_found = course,
+        this.found = course.length
+
+
+      }
+    })
+
+  }
+  else if(this.searchingBy == 'coach'){
+    this.service.getByCoach(this.param).subscribe({
+      next: (course) => {
+        this.c_found = course,
+        this.found = course.length
+
+      }
+    })
+  }
+  else {
+    this.service.getByLevel(this.param).subscribe({
+      next: (course) => {
+        this.c_found = course,
+        this.found = course.length
+
+      }
+    })
+  }
+
+}
 }
