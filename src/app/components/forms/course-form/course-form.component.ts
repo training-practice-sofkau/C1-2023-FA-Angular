15432a3315f6ed5f1a7943e21d996c360a93e7c3
Difsan <difsanchezru@gmail.com>
2023-02-16 23:54:27 -0500

    CourseForm Completed

--- src/app/components/forms/course-form/course-form.component.ts
+++ src/app/components/forms/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
@@ -11,32 +11,60 @@ import { CourseService } from 'src/app/services/course-service/course.service';
 export class CourseFormComponent implements OnInit {
 
   courseForm: FormGroup = new FormGroup({});
+  hiddeCreateButton: boolean = false;
+  hiddeUpdateButton: boolean = false;
 
   constructor(private builder: FormBuilder,
     private service: CourseService,
+    private router: Router,
     private route: ActivatedRoute) { }
 
 
   ngOnInit(): void {
     this.courseForm = this.builder.group(
       {
-        name: '',
-        coach: '',
-        level: 0,
-        lastUpdated: new Date(0)
+        idDTO: '',
+        nameDTO: '',
+        coachDTO: '',
+        levelDTO: 0,
+        lastUpdatedDTO: new Date(0)
       },
     );
 
     this.route.queryParams.subscribe((info) => {
       if(JSON.stringify(info) !== JSON.stringify({})){
         this.courseForm.setValue({
-          name: JSON.parse(info['data']).name,
-        coach: JSON.parse(info['data']).coach,
-        level: JSON.parse(info['data']).level,
-        lastUpdated: JSON.parse(info['data']).lastUpdated,
+        idDTO: JSON.parse(info['data']).idDTO,
+        nameDTO: JSON.parse(info['data']).nameDTO,
+        coachDTO: JSON.parse(info['data']).coachDTO,
+        levelDTO: JSON.parse(info['data']).levelDTO,
+        lastUpdatedDTO: JSON.parse(info['data']).lastUpdatedDTO,
         })
       }
     })
+    this.courseForm.controls['idDTO'].value === ''?
+    this.hiddeUpdateButton=true:
+    this.hiddeCreateButton=true;
+  }
+
+  ngUpdateCourse(): void{
+    this.service.updateCourse(this.courseForm.value).subscribe(
+      (answer) => {
+        alert("Course was update successfully");
+        console.log(answer);
+        this.router.navigate(["/courses"]);
+      }
+    )
+  }
+
+  ngSaveCourse(): void{
+    this.service.saveCourse(this.courseForm.value).subscribe(
+    (answer) => {
+      alert("Course was added successfully");
+      console.log(answer);
+      this.router.navigate(["/courses"]);
+    }
+  )
   }
 
 }
