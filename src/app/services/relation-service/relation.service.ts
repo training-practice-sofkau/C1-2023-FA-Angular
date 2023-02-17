181ebeb23bda0918f9a2b19624a7816644697c29
isan-san <isan.9.9f@gmail.com>
2023-02-17 02:34:38 -0500

    Add methods to create and remove relations to service

--- src/app/services/relation-service/relation.service.ts
+++ src/app/services/relation-service/relation.service.ts
@@ -4,18 +4,31 @@ import { Observable } from 'rxjs';
 import { Student } from 'src/app/models/student.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RelationService {
   constructor(private http: HttpClient) {}
 
   private api: string = 'http://localhost:8080/sofkau/relation/';
 
-  relationsFromStudent(student : string) : Observable<any> {
+  relationsFromStudent(student: string): Observable<any> {
     return this.http.get(`${this.api}student/${student}`);
   }
-  relationsFromCourse(course : string) : Observable<any> {
+  relationsFromCourse(course: string): Observable<any> {
     return this.http.get(`${this.api}course/${course}`);
   }
-
+  createRelation(courseId: string, studentId: string) {
+    let body = {
+      course: {
+        courseId: courseId,
+      },
+      student: {
+        studentId: studentId,
+      },
+    };
+    return this.http.post(this.api,body);
+  }
+  removeRelation(courseId: string, studentId: string) {
+    return this.http.delete(`${this.api}${courseId}/${studentId}`);
+  }
 }
