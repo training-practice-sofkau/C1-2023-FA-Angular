834bf81151f6b8b8e41d2036538924f6832cd59f
William Serna <williamsernac@hotmail.com>
2023-02-17 04:02:14 -0500

    Delete students from courses

--- src/app/components/cards/course-card/course-card.component.ts
+++ src/app/components/cards/course-card/course-card.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Course } from 'src/app/models/course.model';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-card',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./course-card.component.scss']
 })
 export class CourseCardComponent {
+  constructor(private router: Router, private service: CourseService){}
+  @Input() course: Course = {
+    id: '',
+    name: '',
+    coach: '',
+    level: 0,
+    lastUpdated: new Date(),
+    studentList: []
+  }
+  //students: number = this.course.studentList.length;
+
+  goToForm(){
+    this.router.navigate(['courses/edit'],
+    {queryParams: {
+      data: JSON.stringify(this.course)
+    }
+  })
+  }
+
+  onDelete(){
+    if(this.course.studentList != null){
+      this.service.deleteCourse(this.course.id).subscribe({
+        next: (data) => {},
+        error: (err) =>{
+          console.error('Error '+err)
+        },
+        complete:()=>{},
+      });
+      location.reload();
+    }
+    else {
+      alert('The course should not have students before being deleted')
+    }
+}
 
 }
