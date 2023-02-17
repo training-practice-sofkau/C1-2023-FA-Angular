4d5892e38bb18b43e9a4cfc7055ed241373356e7
Diego Becerra <diegofer_1110@hotmail.com>
2023-02-16 21:58:59 -0500

    search student page

---
+++ src/app/pages/search-student/search-student.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student-service/student.service';
+import { filterStudent } from 'src/app/utils/dataForHtml';
+
+@Component({
+  selector: 'app-search-student',
+  templateUrl: './search-student.component.html',
+  styleUrls: ['./search-student.component.scss']
+})
+export class SearchStudentComponent {
+
+  dataSelector: string[] = filterStudent;
+  studentSearch: Student[] = [];
+
+  constructor(private studentService: StudentService) {}
+
+  searchBy(search: any) {
+    switch (search?.myFilter) {
+      case 'name':
+        this.getByNameFilter(search?.paramSearch);
+        break;
+      case 'identification':
+        this.getByIdNumFilter(search?.paramSearch)
+        break;
+      default:
+        console.log('filter invalid');
+    }
+  }
+
+  getByNameFilter(name: string) {
+    this.studentService.getStudentsByName(name).subscribe({
+      next: (res) => (this.studentSearch = res),
+      error: console.log,
+      complete: console.log,
+    });
+  }
+
+  getByIdNumFilter(name:string){
+    this.studentService.getStudentsByIdNum(name).subscribe({
+      next: (res) => (this.studentSearch = res),
+      error: console.log,
+      complete: console.log,
+    });
+  }
+
+  deleteStudent(id: string) {
+    this.studentService.deleteStudent(id).subscribe({
+      next: (res) => {
+        alert(res);
+      },
+      error: console.log,
+    });
+  }
+
+  updateStudent(editStudent: Student) {
+    this.studentService.updateStudent(editStudent).subscribe({
+      next: (res) => {
+        alert('Student updated');
+      },
+      error: console.log,
+    });
+  }
+
+}
