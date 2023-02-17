3057509a5740b72caf15654dcbd0c443ddd03f08
Juangmcz <juangui-089@hotmail.com>
2023-02-16 21:00:59 -0500

    course attribute was added.

--- src/app/models/student.model.ts
+++ src/app/models/student.model.ts
@@ -1,7 +1,10 @@
+import { Course } from "./course.model";
+
 export interface Student {
   studentId: string;
   name: string;
   idNumber: string;
   age: number;
   mail: string;
+  course?: Course;
 }
