213b172b5ba5fe456cb3a371991e78b494ebf79b
William Serna <williamsernac@hotmail.com>
2023-02-17 04:11:30 -0500

    services for students implemented

--- src/app/services/student-service/student.service.ts
+++ src/app/services/student-service/student.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from 'src/app/models/student.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService{
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  api: string = "http://localhost:8090/students"
+
+  getAll(): Observable<any>{
+    return this.http.get(this.api);
+  }
+
+  getByName(name: string): Observable<any>{
+    return this.http.get(this.api+"/name/"+name)
+  }
+
+  getByIdNum(idNum: string): Observable<any>{
+    return this.http.get(this.api+"/idnum/"+idNum)
+  }
+
+  postStudent(student: Student){
+    return this.http.post(this.api,student)
+  }
+
+  putStudent(student: Student){
+    return this.http.put(this.api,student)
+  }
+
+  deleteStudent(id: string){
+    return this.http.delete(this.api+"/"+id)
+  }
 }
