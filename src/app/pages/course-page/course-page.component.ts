50a42c72470bf473152cbaa9e7958657e435134b
William Serna <williamsernac@hotmail.com>
2023-02-17 04:10:22 -0500

    Course page with pagination

--- src/app/pages/course-page/course-page.component.ts
+++ src/app/pages/course-page/course-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { CourseService } from 'src/app/services/course-service/course.service';
 
 @Component({
   selector: 'app-course-page',
   templateUrl: './course-page.component.html',
   styleUrls: ['./course-page.component.scss']
 })
-export class CoursePageComponent {
+export class CoursePageComponent implements OnInit {
+
+  constructor(private service: CourseService){}
+  l_courses: Course[] = [];
+  total: number = 0;
+  page= 0;
+
+  ngOnInit(): void{
+    this.service.getAll().subscribe({
+      next: (course) => {
+        this.l_courses = course,
+        this.total = course.length
+      },
+      error: (console.log),
+    complete: (console.log)
+    })
+  }
 
 }
