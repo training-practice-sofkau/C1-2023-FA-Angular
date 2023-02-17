7656cd15ade5e2348d6bef022f2651641d1db573
William Serna <williamsernac@hotmail.com>
2023-02-17 04:09:03 -0500

    filter for students

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Student } from 'src/app/models/student.model';
+import { StudentService } from 'src/app/services/student-service/student.service';
+
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +9,36 @@ import { Student } from 'src/app/models/student.model';
   styleUrls: ['./student-list.component.scss']
 })
 export class StudentListComponent {
-  s_founded: Student[] = [];
-  founded: number = 0;
+  constructor(private service: StudentService){}
+  s_found: Student[] = [];
+  s_foundID: any;
+  found: number = 0;
   searchingBy: string = '';
+  param: string = '';
 
+
+ngSearch(){
+  console.log(this.searchingBy)
+  if(this.searchingBy == 'idnum'){
+    this.service.getByIdNum(this.param).subscribe({
+      next: (student) => {
+        this.s_foundID = student,
+        this.found = 1
+        console.log(this.found)
+
+      }
+    })
+
+  }
+  else {
+    this.service.getByName(this.param).subscribe({
+      next: (student) => {
+        this.s_found = student,
+        this.found = student.length
+
+      }
+    })
+  }
+}
 
 }
