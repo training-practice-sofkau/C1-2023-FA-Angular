750448247eff55eb936761650fc9e55345d0700a
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-02-17 09:24:59 -0500

    Added select to set the parameter to do the search, used of asyn and Observable type to get the data properly on course-list.

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { CourseService } from 'src/app/services/course-service/course.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
-  styleUrls: ['./course-list.component.scss']
+  styleUrls: ['./course-list.component.scss'],
 })
 export class CourseListComponent {
+  c_founded: Observable<Course[]> = new Observable<Course[]>();
+  founded: number = 0;
+  searchingBy: string = '';
+  selected_option: string = '';
 
+
+  constructor(private service: CourseService) {}
+
+  ngSearchByParam(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.c_founded = this.service.filterParameterType(
+      this.selected_option,
+      element.value
+    );
+  }
 }
